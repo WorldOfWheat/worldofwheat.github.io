e9776e113fc4792b0c8b56f74f6b508e80f3cf84
小麥 <86335529+WorldOfWheat@users.noreply.github.com>
2024-03-23 03:04:19 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 class Codes {
     constructor() {
         this.urls = {
-            'level1': 'https://raw.githubusercontent.com/WorldOfWheat/fast-array.github.io/main/tables/%E4%B8%80%E7%B4%9A%E7%B0%A1%E7%A2%BC.json',
-            'level2': 'https://raw.githubusercontent.com/WorldOfWheat/fast-array.github.io/main/tables/%E4%BA%8C%E7%B4%9A%E7%B0%A1%E7%A2%BC.json',
-            'special': 'https://raw.githubusercontent.com/WorldOfWheat/fast-array.github.io/main/tables/%E7%89%B9%E5%88%A5%E7%A2%BC.json'
+            'level1': 'https://raw.githubusercontent.com/WorldOfWheat/worldofwheat.github.io/main/tables/%E4%B8%80%E7%B4%9A%E7%B0%A1%E7%A2%BC.json',
+            'level2': 'https://raw.githubusercontent.com/WorldOfWheat/worldofwheat.github.io/main/tables/%E4%BA%8C%E7%B4%9A%E7%B0%A1%E7%A2%BC.json',
+            'special': 'https://raw.githubusercontent.com/WorldOfWheat/worldofwheat.github.io/main/tables/%E7%89%B9%E5%88%A5%E7%A2%BC.json'
         };
     }
     
